ca83ea7289194be499bc9b1a51f9b19f5150a72d
ChandanK <chandank@live.ca>
2016-01-28 10:33:20 -0500

    Fixed puppet-mount module

--- modules/puppet-mount/manifests/init.pp
+++ modules/puppet-mount/manifests/init.pp
@@ -1,29 +1,97 @@
-class puppet-mount 
-(
-	$source = undef,
+# Define mounts
+
+define puppet-mount (
+  $source = undef,
   $dest   = undef,
   $type   = undef,
   $opts   = 'defaults',
   $dump   = 0,
   $passno = 0,
-  $ensure = 'present')
-{
-	class {'puppet-mount::install':
-		source => $source,
-		dest => $dest,
-		type => $opts,
-		opts =>	$opts,
-		dump => $dump,
-		passno => $passno,
-		ensure => $ensure,
-	}
-	class { 'puppet-mount::config':
-		source => $source,
-		dest => $dest,
-		type => $opts,
-		opts =>	$opts,
-		dump => $dump,
-		passno => $passno,
-		ensure => $ensure,
-	}
+  $ensure = 'present'){
+
+  if $source == undef {
+    err('The source parameter is required.')
+  }
+
+  if $dest == undef {
+    err('The dest parameter is required.')
+  }
+
+  if $type == undef {
+    err('The type parameter is required.')
+  }
+
+  case $::operatingsystem {
+    redhat, centos: {
+
+      fstab { "fstab entry for ${source} to ${dest} as ${type}":
+        ensure => $ensure,
+        source => $source,
+        dest   => $dest,
+        type   => $type,
+        opts   => $opts,
+        dump   => $dump,
+        passno => $passno,
+      }
+
+      if $type == 'nfs' {
+        ensure_resource('package', 'nfs-utils', {'ensure' => 'present'})
+        case $::operatingsystemmajrelease {
+          '6': {
+            ensure_resource('package', 'rpcbind', {'ensure' => 'present'})
+            ensure_resource('service', 'rpcbind', {'ensure' => 'running'})
+            Package['rpcbind'] -> Service['rpcbind']
+          }
+          '5': {
+            ensure_resource('package', 'portmap', {'ensure' => 'present'})
+            ensure_resource('service', 'portmap', {'ensure' => 'running'})
+            Package['portmap'] -> Service['portmap']
+          }
+          default: {
+            alert('Unsupported version of OS')
+          }
+        }
+      }
+
+      case $ensure {
+        'present': {
+          # Ensure the entire tree of the destination has been created.
+          $dirtree = dirtree($dest)
+          ensure_resource('file', $dirtree, {'ensure' => 'directory'})
+
+          exec { "/bin/mount '${dest}'":
+            unless  => "/bin/mount -l | /bin/grep '${dest}'",
+            require => [File[$dirtree], Fstab["fstab entry for ${source} to ${dest} as ${type}"]],
+          }
+
+        
+		  exec {'mount-command':
+	        command => '/bin/mount -a',
+		    refreshonly => true,
+          }
+		  $conf_dir="/etc/mountpoint_cfg"
+			file { '/etc/mountpoint_cfg':
+            ensure => directory,
+		  }
+          file {"${conf_dir}/mount.cfg":
+            ensure => present,
+            content => template('puppet-mount/mount.cfg.erb'),
+            notify => Exec['mount-command'],
+		  }
+		}
+		'absent': {
+          exec { "/bin/umount '${dest}'":
+            onlyif => "/bin/mount -l | /bin/grep '${dest}'",
+            before => Fstab["fstab entry for ${source} to ${dest} as ${type}"],
+          }
+
+          # Note: we won't remove the directory since we don't know if it'll destroy data
+          notify { "${dest} wasn't removed after being unmounted.  Please remove it manually.": }
+        }
+        default: { }
+      }
+    }
+    default: { err('Your OS isn\'t supported by the mounts module yet.') }
+  }
+
 }
